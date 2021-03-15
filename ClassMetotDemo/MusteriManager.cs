d247138fe968379bede016004111b4af78060ceb
ahmetmete.2477 <ahmetmete.2477@gmail.com>
2021-03-15 17:52:47 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Yeni müşteri eklendi : " + musteri.Ad + musteri.Soyad);
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri çıkarıldı : "+ musteri.Ad + musteri.Soyad);
+        }
+
+        public void Listeleme(Musteri[] musteriler)
+        {
+            Console.WriteLine("Müsterilerimiz");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad + musteri.Soyad);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ali";
+            musteri1.Soyad = "Mutlu";
+            musteri1.TelNo = "55541111";
+            musteri1.HesapTuru = "Vadesiz";
+            musteri1.Id = 553;
+            
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Ayşe";
+            musteri2.Soyad = "Varlı";
+            musteri2.TelNo = "55141221";
+            musteri2.HesapTuru = "Vadeli";
+            musteri2.Id = 554;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Tahsin";
+            musteri3.Soyad = "Zorlu";
+            musteri3.TelNo = "55841411";
+            musteri3.HesapTuru = "Vadesiz";
+            musteri3.Id = 555;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine("------Musteri Ekleme-------");
+            musteriManager.Ekle(musteri1);
+
+            Console.WriteLine("-------------Müsteri Silme-------");
+            musteriManager.Silme(musteri2);
+
+            Console.WriteLine("--------Listeleme---------------");
+            musteriManager.Listeleme(musteriler);
+
+
+
+
+
+        }
+    }
+}
